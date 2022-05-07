3183c7e4d792760e4c043e9809ee996b8fe2ce0c
Ankur Sarkar <78259188+Ankur02Sarkar@users.noreply.github.com>
2022-05-07 06:56:03 +0530

    Create code.cpp

---
+++ 0001 - Reverse a String/code.cpp
@@ -0,0 +1,14 @@
+string reverseWord(string str){
+    /* APPROACH 1 */
+    
+    // for(int i = str.length(); i > 0 ; i--){
+    //     cout<<str[i-1];
+    // }
+    
+    /* APPROACH 2 */
+    int first_half = str.length() / 2;
+    for(int i = 0 ; i < first_half ; i++){
+        swap(str[i], str[str.length() - 1 - i]);
+    }
+    return str;
+}
