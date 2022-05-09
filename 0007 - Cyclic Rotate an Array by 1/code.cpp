6c3f517c42e9776a71dbc13d1856fe0b821a6d1c
Ankur Sarkar <78259188+Ankur02Sarkar@users.noreply.github.com>
2022-05-09 07:22:03 +0530

    Create code.cpp

---
+++ 0007 - Cyclic Rotate an Array by 1/code.cpp
@@ -0,0 +1,13 @@
+/*
+Question Link:-
+https://practice.geeksforgeeks.org/problems/cyclically-rotate-an-array-by-one2614/1
+*/
+
+void rotate(int arr[], int n)
+{
+    int tmp = arr[n-1];
+    for(int i=n-1; i>0 ; i--){
+        arr[i]=arr[i-1];
+    }
+    arr[0] = tmp;
+}
